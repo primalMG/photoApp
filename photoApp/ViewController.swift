190e8b1fc30c5016b58f5fce0db9fcc6695f451f
user136368 <user136368@EU251.local>
2018-10-10 16:10:39 +0100

    figuring out instagrams api as well as the back end

--- photoApp/ViewController.swift
+++ photoApp/ViewController.swift
@@ -8,11 +8,47 @@
 
 import UIKit
 
+struct Img: Decodable {
+   let data: [Data]
+}
+
+struct Data: Decodable {
+    let id: String
+    let type: String
+    let filter: String
+    let link: String
+    let images: Imgs
+}
+
+struct Imgs: Decodable {
+    let standard_resolution: Urls
+}
+
+struct Urls: Decodable {
+    let url: String
+}
+
 class ViewController: UIViewController {
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let urlString = "https://api.instagram.com/v1/users/self/media/recent/?access_token=465172273.1677ed0.0413561183774706a2a5ddf38cd564c7"
+        guard let url = URL(string: urlString) else { return }
+        
+        URLSession.shared.dataTask(with: url) { (data, response, error) in
+            guard let data = data else { return }
+            
+            do {
+                let images = try JSONDecoder().decode(Img.self, from: data)
+                print(images)
+            } catch {
+                let error = error
+                print(error)
+            }
+        }.resume()
+        
     }
 
     override func didReceiveMemoryWarning() {
