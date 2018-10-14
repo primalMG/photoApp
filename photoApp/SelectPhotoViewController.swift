10c9aa80294ca367fdbba20deca1f317acc2ea2b
user136368 <user136368@EU251.local>
2018-10-14 17:40:59 +0100

    made changes to the way the drawing works, so I was able to change the way the share button works

--- photoApp/SelectPhotoViewController.swift
+++ photoApp/SelectPhotoViewController.swift
@@ -8,9 +8,16 @@
 
 import UIKit
 
+
 class SelectPhotoViewController: UIViewController {
 
     var selectedImage: Urls?
+    var lineColour = UIColor.black
+    var lineWidth: CGFloat = 5.0
+    var endPoint = CGPoint.zero
+    var swipe = false
+    var opacity: CGFloat = 1.0
+    
     
     @IBOutlet weak var imageView: UIImageView! {
         didSet {
@@ -20,11 +27,13 @@ class SelectPhotoViewController: UIViewController {
         }
     }
     
+    @IBOutlet weak var drawImageView: UIImageView!
+    
     @IBAction func photoShare(_ sender: Any) {
         UIGraphicsBeginImageContext(view.frame.size)
         view.layer.render(in: UIGraphicsGetCurrentContext()!)
         
-        guard let image = UIGraphicsGetImageFromCurrentImageContext() else { return }
+        guard let image = imageView.image else { return }
         
         let activityVC = UIActivityViewController(activityItems: [image], applicationActivities: nil)
         
@@ -33,7 +42,53 @@ class SelectPhotoViewController: UIViewController {
         present(activityVC, animated: true, completion: nil)
     }
     
+    @IBAction func btnReset(_ sender: Any) {
+        drawImageView.image = nil
+    }
     
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        guard let touch = touches.first else { return }
+        endPoint = touch.location(in: view)
+        swipe = false
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        guard let touch = touches.first else { return }
+        swipe = true
+        let currentPoint = touch.location(in: view)
+        DrawShape(from: endPoint, to: currentPoint)
+        endPoint = currentPoint
+    }
+    
+    func DrawShape(from startPoint: CGPoint, to endPoint: CGPoint){
+        UIGraphicsBeginImageContext(view.frame.size)
+        guard let context = UIGraphicsGetCurrentContext() else { return }
+        imageView.image?.draw(in: view.bounds)
+        
+        context.move(to: startPoint)
+        context.addLine(to: endPoint)
+        context.setLineCap(.square)
+        context.setBlendMode(.normal)
+        context.setLineWidth(lineWidth)
+        context.setStrokeColor(colourWheel().cgColor)
+        
+        context.strokePath()
+        
+        imageView.image = UIGraphicsGetImageFromCurrentImageContext()
+        imageView.alpha = opacity
+        
+        UIGraphicsEndImageContext()
+    }
+    
+    
+    func random () -> CGFloat {
+        
+        return CGFloat(arc4random()) / CGFloat(UInt32.max)
+    }
+    
+    func colourWheel () -> UIColor {
+        return UIColor(red: self.random(), green: self.random(), blue: self.random(), alpha: 1.0)
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
 
